2f2ce6798aa751ad5916a1a548791ddaccd8b866
Kasra Hosseini <kasrakoosha@yahoo.com>
2023-04-27 03:29:26 +0330

    feat: add react router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { action as registerAction, Register } from './views/Register'
+import { loader as rootLoader, Dashboard } from './views/Dashboard'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    loader: rootLoader,
+    element: <Dashboard />,
+  },
+  {
+    path: '/register',
+    action: registerAction,
+    element: <Register />,
+  },
+])
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
