18948fb958b98107862f5e33156e48a0ba0896bf
Kasra Hosseini <kasrakoosha@yahoo.com>
2023-07-04 13:05:08 +0330

    feat: add formik with validation to login and register page

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -5,6 +5,8 @@ import Typography from '@mui/material/Typography'
 import { Form, redirect, Link } from 'react-router-dom'
 import { login } from '../api/users'
 import { useTranslation } from 'react-i18next'
+import { Formik, Field } from 'formik'
+import * as yup from 'yup'
 
 export async function action({ request }) {
   const formData = await request.formData()
@@ -12,44 +14,88 @@ export async function action({ request }) {
   const user = await login(data)
   return redirect('/')
 }
+
+const validationSchema = yup.object({
+  email: yup
+    .string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: yup
+    .string('Enter your password')
+    .min(6, 'Password should be of minimum 8 characters length')
+    .required('Password is required'),
+})
+
 export const Login = () => {
   const { t } = useTranslation()
   return (
-    <Form method="post">
-      <Box
-        sx={{
-          display: 'flex',
-          p: 4,
-          border: 1,
-          flexDirection: 'column',
-          gap: 1,
-          width: '50%',
-          margin: '16px auto',
+    <Box
+      sx={{
+        display: 'flex',
+        p: 4,
+        border: 1,
+        flexDirection: 'column',
+        width: '50%',
+        margin: '16px auto',
+      }}
+    >
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
         }}
+        validationSchema={validationSchema}
       >
-        <TextField name="email" label={t('login.email')} variant="outlined" />
-        <TextField
-          name="password"
-          label={t('login.password')}
-          variant="outlined"
-        />
-        <Button type="submit" variant="contained">
-          {t('login.login')}
-        </Button>
-        <Typography>
-          {t("login.don't have an account?")}
-          <Link
-            to="/register"
-            style={{
-              textDecoration: 'none',
-              color: 'black',
-              fontWeight: 'bold',
-            }}
-          >
-            {t('login.register now')}
-          </Link>
-        </Typography>
-      </Box>
-    </Form>
+        {({ errors, touched, isValid, dirty }) => (
+          <Form method="post">
+            <Field
+              fullWidth
+              name="email"
+              type="email"
+              as={TextField}
+              variant="outlined"
+              label={t('login.email')}
+              error={touched.email && Boolean(errors.email)}
+              helperText={touched.email && errors.email}
+              sx={{ mb: 1 }}
+            />
+
+            <Field
+              fullWidth
+              name="password"
+              type="password"
+              as={TextField}
+              variant="outlined"
+              label={t('login.password')}
+              error={touched.password && Boolean(errors.password)}
+              helperText={touched.password && errors.password}
+              sx={{ mb: 1 }}
+            />
+
+            <Button
+              variant="contained"
+              type="submit"
+              disabled={!dirty || !isValid}
+              sx={{ mb: 1 }}
+            >
+              Submit
+            </Button>
+            <Typography>
+              {t("login.don't have an account?")}
+              <Link
+                to="/register"
+                style={{
+                  textDecoration: 'none',
+                  color: 'black',
+                  fontWeight: 'bold',
+                }}
+              >
+                {t('login.register now')}
+              </Link>
+            </Typography>
+          </Form>
+        )}
+      </Formik>
+    </Box>
   )
 }
--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -5,6 +5,8 @@ import Typography from '@mui/material/Typography'
 import { Form, redirect, Link } from 'react-router-dom'
 import { register } from '../api/users'
 import { useTranslation } from 'react-i18next'
+import { Formik, Field } from 'formik'
+import * as yup from 'yup'
 
 export async function action({ request }) {
   const formData = await request.formData()
@@ -12,53 +14,103 @@ export async function action({ request }) {
   const user = await register(data)
   return redirect('/')
 }
+const validationSchema = yup.object({
+  email: yup
+    .string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  username: yup
+    .string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Username is Required'),
+  password: yup
+    .string('Enter your password')
+    .min(6, 'Password should be of minimum 8 characters length')
+    .required('Password is required'),
+})
+
 export const Register = () => {
   const { t } = useTranslation()
   return (
-    <Form method="post">
-      <Box
-        sx={{
-          display: 'flex',
-          p: 4,
-          border: 1,
-          flexDirection: 'column',
-          gap: 1,
-          width: '50%',
-          margin: '16px auto',
+    <Box
+      sx={{
+        display: 'flex',
+        p: 4,
+        border: 1,
+        flexDirection: 'column',
+        width: '50%',
+        margin: '16px auto',
+      }}
+    >
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
         }}
+        validationSchema={validationSchema}
       >
-        <TextField
-          name="email"
-          label={t('register.email')}
-          variant="outlined"
-        />
-        <TextField
-          name="password"
-          label={t('register.password')}
-          variant="outlined"
-        />
-        <TextField
-          name="username"
-          label={t('register.username')}
-          variant="outlined"
-        />
-        <Button type="submit" variant="contained">
-          {t('register.register')}
-        </Button>
-        <Typography>
-          {t('register.already registered?')}
-          <Link
-            to="/login"
-            style={{
-              textDecoration: 'none',
-              color: 'black',
-              fontWeight: 'bold',
-            }}
-          >
-            {t('register.login')}
-          </Link>
-        </Typography>
-      </Box>
-    </Form>
+        {({ errors, touched, isValid, dirty }) => (
+          <Form method="post">
+            <Field
+              fullWidth
+              name="username"
+              type="text"
+              as={TextField}
+              variant="outlined"
+              label={t('register.username')}
+              error={touched.username && Boolean(errors.username)}
+              helperText={touched.username && errors.username}
+              sx={{ mb: 1 }}
+            />
+            <Field
+              fullWidth
+              name="email"
+              type="email"
+              as={TextField}
+              variant="outlined"
+              label={t('login.email')}
+              error={touched.email && Boolean(errors.email)}
+              helperText={touched.email && errors.email}
+              sx={{ mb: 1 }}
+            />
+
+            <Field
+              fullWidth
+              name="password"
+              type="password"
+              as={TextField}
+              variant="outlined"
+              label={t('register.password')}
+              error={touched.password && Boolean(errors.password)}
+              helperText={touched.password && errors.password}
+              sx={{ mb: 1 }}
+            />
+
+            <Button
+              variant="contained"
+              type="submit"
+              disabled={!dirty || !isValid}
+              sx={{ mb: 1 }}
+            >
+              Submit
+            </Button>
+            <Typography>
+              {t('register.already registered?')}
+              <Link
+                to="/login"
+                style={{
+                  textDecoration: 'none',
+                  color: 'black',
+                  fontWeight: 'bold',
+                }}
+              >
+                {t('register.login')}
+              </Link>
+            </Typography>
+          </Form>
+        )}
+      </Formik>
+    </Box>
   )
 }
